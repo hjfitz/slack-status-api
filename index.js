03073cd7ea41cc2922704a1263c8c3105d33b313
Harry <hfitz83@gmail.com>
2018-07-15 19:45:08 +0100

    it works, now to add caching

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const scrape = require('./scraper');
 
 const api = express();
 
-api.get('/', (req, res) => {
-
-});
+api.get('/', (req, res) => scrape().then(status => res.json(status)));
 
 api.listen(process.env.PORT || 5000, async () => {
   console.log('api listening on', process.env.PORT);
--- scraper.js
+++ scraper.js
@@ -4,12 +4,23 @@ const debug = require('debug')('scrape:scraper');
 
 const baseurl = 'https://status.slack.com/';
 
+const iconLookup = {
+  '/img/v2/TableCheck.png': 'No Issues',
+  '/img/v2/TableMaintenance.png': 'Maintenance',
+  '/img/v2/TableNotice.png': 'Notice',
+  '/img/v2/TableIncident.png': 'Incident',
+  '/img/v2/TableOutage.png': 'Outage',
+};
+
 const scrape = async () => {
   debug('attempting to GET', baseurl);
   const { data } = await axios.get(baseurl);
+  debug('Retrieved HTML, converting to DOM');
   const dom = new JSDOM(data);
+  debug('parsed to DOM');
 
   // sometimes there's no main status
+  debug('attempting to get main status');
   let main;
   try {
     main = dom.window.document.getElementsByTagName('h1')[0].textContent;
@@ -17,8 +28,25 @@ const scrape = async () => {
     main = 'No Status';
   }
 
-  const services = Array.from(dom.window.document.querySelectorAll('.service.header.align_center'));
+  debug('parsing remaining status');
+  const services = Array
+  .from(dom.window.document.querySelectorAll('.service.header.align_center'))
+  .reduce((acc, serviceDOM) => {
+    const [service, stat] = serviceDOM.children;
+    const serviceName = service.children[0].textContent;
+    debug('serviceName get', serviceName);
+    const status = stat.children[0].src;
+    debug('image get', status);
+    acc[serviceName] = iconLookup[status];
+    return acc;
+  }, {});
+  return {
+    status: main,
+    services,
+  }
 };
+
+scrape().then(console.log);
 
 
 module.exports = scrape;
